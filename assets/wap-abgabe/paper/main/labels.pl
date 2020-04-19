6fff8b92e8a8d59d74f2dcac062542c542abc382
Christian Mösl <christian.moesl@live.at>
2020-04-19 21:54:31 +0200

    Test latex2html paper

---
+++ assets/wap-abgabe/paper/main/labels.pl
@@ -0,0 +1,17 @@
+# LaTeX2HTML 2020 (Released January 1, 2020)
+# Associate labels original text with physical files.
+
+
+1;
+
+
+# LaTeX2HTML 2020 (Released January 1, 2020)
+# labels from external_latex_labels array.
+
+
+$key = q/_/;
+$external_latex_labels{$key} = q|<|; 
+$noresave{$key} = "$nosave";
+
+1;
+
